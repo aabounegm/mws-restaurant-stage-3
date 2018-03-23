50f18cb10d170e9b97a6e94ccb373f32b9c34420
AbdelRahman Aly <abounegm.abdelrahman@gmail.com>
2018-03-24 01:36:04 +0200

    Created gulp tasks and set environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,69 @@
 /* eslint-env node */
 const gulp = require('gulp');
-gulp.task('default', function() {
-	
+const minify = require('gulp-minify');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass');
+const environment = require('gulp-environments');
+const sourcemaps = require('gulp-sourcemaps');
+const webserver = require('gulp-webserver');
+
+const production = environment.production;
+const development = environment.development;
+
+gulp.task('sass', function() {
+	return gulp.src('./src/scss/**/*.scss')
+		.pipe(development(sourcemaps.init()))
+		.pipe(sass({
+			outputStyle: production() ? 'compressed' : 'expanded',
+			indentType: 'tab',
+			indentWidth: 1
+		}).on('error', sass.logError))
+		.pipe(development(sourcemaps.write()))
+		.pipe(gulp.dest('./dist/css/'));
 });
+gulp.task('sass:watch', ['sass'], function() {
+	return gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('js:minify', function() {
+	return gulp.src('./src/js/**/*.js')
+		// .pipe(development(sourcemaps.init()))
+		.pipe(production(minify({
+			noSource: true,
+			ext: {
+				min: '.js'
+			}
+		})))
+		// .pipe(development(sourcemaps.write()))
+		.pipe(gulp.dest('./dist/js/'));
+});
+gulp.task('js:watch', ['js:minify'], function() {
+	return gulp.watch('./src/js/**/*.js', ['js:minify']);
+});
+
+gulp.task('webserver', function() {
+	return gulp.src('dist')
+		.pipe(webserver({
+			fallback: 'index.html'
+		}));
+});
+
+gulp.task('root:copy', function() {
+	return gulp.src('./src/*.*')
+		.pipe(gulp.dest('./dist/'));
+});
+gulp.task('root:watch', ['root:copy'], function() {
+	gulp.watch('./src/*.*', ['root:copy']);
+});
+gulp.task('imagemin', function() {
+	return gulp.src('./src/img/*')
+		.pipe(imagemin({verbose:false}))
+		.pipe(gulp.dest('./dist/img/'));
+});
+
+
+gulp.task('set-dev', development.task);
+gulp.task('set-pro', production.task);
+
+gulp.task('default', ['set-dev', 'imagemin', 'sass:watch', 'js:watch', 'root:watch', 'webserver']);
+gulp.task('build', ['set-pro', 'imagemin', 'sass', 'js:minify', 'root:copy']);
